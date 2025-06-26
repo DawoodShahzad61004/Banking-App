c2440b22605126eead961bad8f5193091f0de0d9
DawoodShahzad61004 <dawoodbutt590@gmail.com>
2025-06-26 06:14:12 +0500

    Implement SignupPage component and update App to render it

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import LoginPage from './containers/loginScreens/login_page'
+import SignupPage from './containers/loginScreens/signup_page'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <LoginPage/>
+      // <LoginPage/>
+      <SignupPage/> 
   )
 }
 
--- Frontend/src/components/inputField.jsx
+++ Frontend/src/components/inputField.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function InputField(props) {
-
+  let inputClass = "w-" + (props.width || "full") + " max-w-[400px] px-[16px] py-[12px] border-[2px] border-[#d1d5db] rounded-[8px] focus:border-[#3b82f6] focus:outline-none text-[black] bg-[#f9fafb] focus:bg-[#ffffff] transition-all duration-[200ms] text-[16px]";
   return (
     <div>
       <label
@@ -17,7 +17,7 @@ function InputField(props) {
         value={props.value || ""}
         onChange={props.changeHandler || (() => {})}
         required
-        className="w-full max-w-[400px] px-[16px] py-[12px] border-[2px] border-[#d1d5db] rounded-[8px] focus:border-[#3b82f6] focus:outline-none text-[black] bg-[#f9fafb] focus:bg-[#ffffff] transition-all duration-[200ms] text-[16px]"
+        className={`${inputClass}`}
         placeholder={props.placeholder || "input-placeholder"}
       />
     </div>
